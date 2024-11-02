cce2ba5ad18ed3fcd73bd3304405862131f466b0
Lee Salminen <leesalminen@gmail.com>
2024-11-02 14:40:05 -0600

    tweak demo

--- back_end/index.js
+++ back_end/index.js
@@ -4,10 +4,8 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
 import cors from 'cors'
-import axios from 'axios'
 import serveStatic from 'serve-static'
 import * as dotenv from 'dotenv'
-import { createHash, createHmac } from 'crypto'
 
 dotenv.config({ path: './back_end/.env' })
 
@@ -31,7 +29,7 @@ app.use(
       directives: {
         "connect-src": ["'self'", apiUrl, authUrl, ],
         "script-src": ["'self'", 'blob:', "'sha256-BIcUshLJeO09OXe5RD8UZOn1AV4nYRvcvT5vXbf8lyw='"],
-        "frame-ancestors": ["'self'", "https://wallet.bitcoinjungle.app", "http://localhost:3100"],
+        "frame-ancestors": ["'self'", "https://coinos.io", "http://localhost:3100"],
         "img-src": ["'self'", 'data:', 'https://cdnjs.cloudflare.com'],
         "frame-src": ["'self'", 'https://*.incodesmile.com'],
         "object-src": ["'self'"],
